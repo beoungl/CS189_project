b2b3c755a30537ddf6237a8db7dbba2e6a0b99da
beoungl <43657684+beoungl@users.noreply.github.com>
2019-06-14 20:15:32 -0700

    Create gen_n50s.sh

---
+++ gen_n50s.sh
@@ -0,0 +1,10 @@
+source ~/.minicondatest3rc
+conda activate project1
+cd /pub/beoungl/CS189_project
+mkdir assembly_stats
+assembly-stats data/SK1_reference.fasta > assembly_stats/SK1_reference.stats
+assembly-stats data/S288C_reference.fasta > assembly_stats/S288C_reference.stats
+assembly-stats data/suvarum_cbs7001.fasta > assembly_stats/suvarum_cbs7001.stats
+assembly-stats data/g833-1B_reference.fasta > assembly_stats/g833-1B_reference.stats
+assembly-stats data/W303_reference.fasta > assembly_stats/W303_reference.stats
+conda deactivate
